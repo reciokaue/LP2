e710f558e9e6140441fe8d346bd2df3e1275883a
Kaue Recio Carpim Sala <kaue.recio2@gmail.com>
2023-04-11 12:32:51 -0300

    Add files via upload

---
+++ atividade 6/FormularioExercicio3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Menu
+{
+    public partial class FormularioExercicio3 : Form
+    {
+        public FormularioExercicio3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string entrada = textBox2.Text;
+            string trocar= textBox1.Text;
+            string resultado = "";
+
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                bool teste = entrada.Substring(i, trocar.Length) == trocar;
+                if (teste)
+                {
+                    entrada = entrada.Substring(i + trocar.Length, entrada.Length - i - trocar.Length);
+                }
+                else
+                {
+                    resultado = entrada;
+                }
+
+            }
+            
+            textBox2.Text = resultado;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length > 0)
+            {
+                textBox2.Text = textBox2.Text.Replace(textBox1.Text, "");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            char[] texto1 = textBox1.Text.ToCharArray();
+            Array.Reverse(texto1);
+            textBox2.Text = new string(texto1);
+        }
+    }
+}
