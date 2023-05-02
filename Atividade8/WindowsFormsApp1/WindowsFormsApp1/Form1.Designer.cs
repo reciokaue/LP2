0618a04424c22a832ea9bc81b9f6370489c87533
Kaue Recio Carpim Sala <kaue.recio2@gmail.com>
2023-05-02 09:19:08 -0300

    Add files via upload

---
+++ Atividade8/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Função void";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(66, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 77);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Função return";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(548, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 77);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Função void parametro";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(66, 199);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(182, 77);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Função return parametro";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(311, 199);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(182, 77);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Função void ref";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(548, 199);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(182, 77);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Função return e ref";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(66, 87);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(182, 77);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Função out";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(311, 87);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(182, 77);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Função out return";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(548, 87);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(182, 77);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Função out opcional param";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Atividade8/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        void Soma1()
+        {
+            Double resultado = 1 + 1;
+            MessageBox.Show(resultado.ToString());
+        }
+        double Soma2()
+        {
+            return 1 + 1;
+        }
+        void Soma3(double x, double y)
+        {
+            x = 40;
+            double resultado = x + y;
+            MessageBox.Show("O resultado é: " + resultado.ToString());
+        }
+        double Soma4(double x, double y)
+        {
+            return x + y;
+        }
+        void Soma5(ref double resultado, ref double a)
+        {
+            a = 4;
+            resultado = 2 + 3;
+        }
+        double Soma6 (ref double resultado){
+            resultado = 2 + 3;
+            return resultado;
+        }
+        void Soma7(out double resultado)
+        {
+            resultado = 2 + 3;
+        }
+        double Soma8(out double resultado)
+        {
+            return resultado = 2 + 3;
+        }
+        void Soma9(out double retorno, double x ,double y, double z = 0)
+        {
+            if (z != 0)
+                retorno = (x + y + z) / 3;
+            else
+                retorno = (x + y) / 2;
+        }
+
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //função void
+            Soma1();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //função return
+            MessageBox.Show(Soma2().ToString());
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //função void parametro
+            Soma3(1, 10);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            // função return parametro
+            MessageBox.Show(Soma4(10, 20).ToString());
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            double a = 10;
+            double b = 20;
+
+            Soma5(ref b, ref a);
+
+            MessageBox.Show("O resultado é " + b + "valor de b " + a);
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            double resultado = 10;
+
+            Soma6(ref resultado);
+
+            MessageBox.Show("O resultado é " + resultado + "o return é" + Soma6(ref resultado));
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            double a = 50;
+            Soma7(out a);
+            MessageBox.Show(a.ToString());
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            double a = 50;
+            double b = Soma8(out a);
+            MessageBox.Show("valor a: " + a + " Valor b: " + b);
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            double a = 10, b = 10;
+
+            Soma9(out a, a, b);
+
+            MessageBox.Show("Valor resultado:" + a);
+        }
+    }
+}
